0de26561db3eaee5ce5da121dffd35d881e8e6a9
Juan J. Martinez <jjm@usebox.net>
2020-06-07 08:43:53 +0100

    version bump

--- png2c.py
+++ png2c.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 """
-__version__ = "1.4.1"
+__version__ = "1.4.3"
 
 from argparse import ArgumentParser
 from PIL import Image

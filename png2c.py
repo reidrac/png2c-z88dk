05a2cb9269ded148815334479f11a9fbe7c4a6a5
Juan J. Martinez <jjm@usebox.net>
2020-06-07 08:40:37 +0100

    typo, formatting

--- png2c.py
+++ png2c.py
@@ -27,34 +27,35 @@ __version__ = "1.4.1"
 from argparse import ArgumentParser
 from PIL import Image
 
-COLORS = ( (0, 0, 0),
-           (0, 0, 205), (0, 0, 255),
-           (205, 0, 0), (255, 0, 0),
-           (205, 0, 205), (255, 0, 255),
-           (0, 205, 0), (0, 255, 0),
-           (0, 205, 205), (0, 255, 255),
-           (205, 205, 0), (255, 255, 0),
-           (205, 205, 205), (255, 255, 255),
-           )
+COLORS = ((0, 0, 0),
+          (0, 0, 205), (0, 0, 255),
+          (205, 0, 0), (255, 0, 0),
+          (205, 0, 205), (255, 0, 255),
+          (0, 205, 0), (0, 255, 0),
+          (0, 205, 205), (0, 255, 255),
+          (205, 205, 0), (255, 255, 0),
+          (205, 205, 205), (255, 255, 255),
+          )
 
-COLOR_NAMES = ( "black", "blue", "bright-blue",
-                "red", "bright-red", "magenta", "bright-magenta",
-                "green", "bright-green", "cyan", "bright-cyan",
-                "yellow", "bright-yellow", "white", "bright-white",)
+COLOR_NAMES = ("black", "blue", "bright-blue",
+               "red", "bright-red", "magenta", "bright-magenta",
+               "green", "bright-green", "cyan", "bright-cyan",
+               "yellow", "bright-yellow", "white", "bright-white",)
 
 
-ATTR_I = ( 0x00, 0x01, 0x01 | 0x40, 0x02, 0x02 | 0x40,
-           0x03, 0x03 | 0x40, 0x04, 0x04 | 0x40, 0x05, 0x05 | 0x40,
-           0x06, 0x06 | 0x40, 0x07, 0x07 | 0x40,)
+ATTR_I = (0x00, 0x01, 0x01 | 0x40, 0x02, 0x02 | 0x40,
+          0x03, 0x03 | 0x40, 0x04, 0x04 | 0x40, 0x05, 0x05 | 0x40,
+          0x06, 0x06 | 0x40, 0x07, 0x07 | 0x40,)
 
-ATTR_P = ( 0x00, 0x08, 0x08 | 0x40, 0x10, 0x10 | 0x40,
-           0x18, 0x18 | 0x40, 0x20, 0x20 | 0x40, 0x28, 0x28 | 0x40,
-           0x30, 0x30 | 0x40, 0x38, 0x38 | 0x40,)
+ATTR_P = (0x00, 0x08, 0x08 | 0x40, 0x10, 0x10 | 0x40,
+          0x18, 0x18 | 0x40, 0x20, 0x20 | 0x40, 0x28, 0x28 | 0x40,
+          0x30, 0x30 | 0x40, 0x38, 0x38 | 0x40,)
 
 C2I = dict(zip(COLORS, ATTR_I))
 C2P = dict(zip(COLORS, ATTR_P))
 
 BASE = 128
+
 
 def main():
 
@@ -62,7 +63,8 @@ def main():
                             epilog="Copyright (C) 2014 Juan J Martinez <jjm@usebox.net>",
                             )
 
-    parser.add_argument("--version", action="version", version="%(prog)s "  + __version__)
+    parser.add_argument("--version", action="version",
+                        version="%(prog)s " + __version__)
     parser.add_argument("-b", "--base", dest="base", default=BASE, type=int,
                         help="base character (default: %d)" % BASE)
     parser.add_argument("-i", "--id", dest="id", default="tiles", type=str,
@@ -108,10 +110,11 @@ def main():
         parser.error("%r size is not multiple of 8" % args.image)
 
     if not isinstance(image.getpixel((0, 0)), tuple):
-        parse.error("only RGB(A) images are supported")
+        parser.error("only RGB(A) images are supported")
 
     # so we support both RGB and RGBA images
-    data = list(zip(list(image.getdata(0)), list(image.getdata(1)), list(image.getdata(2))))
+    data = list(zip(list(image.getdata(0)), list(
+        image.getdata(1)), list(image.getdata(2))))
 
     for c in data:
         if c not in COLORS:
@@ -138,7 +141,8 @@ def main():
                 byte.append(row)
 
             if len(attr) > 2:
-                parser.error("more than 2 colors in an attribute block in (%d, %d)" % (x, y))
+                parser.error(
+                    "more than 2 colors in an attribute block in (%d, %d)" % (x, y))
             elif len(attr) < 2:
                 attr.append(COLORS[0])
 
@@ -203,7 +207,8 @@ def main():
     for part in range(0, len(print_str), 8):
         if print_out:
             print_out += ",\n"
-        print_out += ', '.join(["0x%02x" % c for c in print_str[part:part + 8]])
+        print_out += ', '.join(["0x%02x" %
+                                c for c in print_str[part:part + 8]])
 
     # header
     print("""
@@ -225,9 +230,9 @@ def main():
 #define %s_BASE %d
 #define %s_LEN %d
 uchar %s[] = {\n%s};
- """ % (args.id.upper(),args.base, args.id.upper(), len(tiles),
+ """ % (args.id.upper(), args.base, args.id.upper(), len(tiles),
         args.id, out,))
+
 
 if __name__ == "__main__":
     main()
-
